cad45c89174f6d922415ee0d76b4600f28f20e51
AmonovSokhib <amonovsokhib@gmail.com>
2022-07-04 15:35:00 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var name = prompt("Ismingizni kiriting...")
+var surname = prompt("Familyangizni kiriting...")
+var age = Number(prompt("Yoshingizni kiriting..."))
+var telefon = Number(prompt("Telefon raqamingizni kiriting..."))
+
+console.log(` Ismi: ${name} \n Famiyasi: ${surname} \n Yoshi:  ${age} \n Tel: +(998)${telefon}`);
+alert(` Ismi: ${name} \n Famiyasi: ${surname} \n Yoshi:  ${age} \n Tel: +(998)${telefon}`)
